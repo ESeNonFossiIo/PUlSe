89edb5c155375e4c7f1befae7dc800f31a0a64c0
ESeNonFossiIo <esenonfossiio@gmail.com>
2017-12-05 14:43:49 +0100

    Add str2bool

--- lib/PUlSe_string.py
+++ lib/PUlSe_string.py
@@ -2,18 +2,18 @@
   This is a Class to deal with strings.
 """
 
-def get_name_inside(  line, 
-                      begin_container="(", 
+def get_name_inside(  line,
+                      begin_container="(",
                       end_container=")"):
   """ Get the value inside a container.
-  
+
     Capture the text contained between two containers.
 
     Args:
       line (str): Text string
       begin_container (str): This delimiter is the one before the value to capture.
       end_container (str): This delimiter is the one after the value to capture.
-      
+
     Returns:
       string: value contained between `begin_container` and `end_container`.
   """
@@ -23,23 +23,34 @@ def get_name_inside(  line,
   start = line.find(begin_container)+size
   end = line.find(end_container, start)
   if end > -1 and start > -1:
-    return line[start:end] 
+    return line[start:end]
   else :
     return ""
 
-def fill_dictionary_with_config_parser( dictionary, 
-                                        confparser, 
+def fill_dictionary_with_config_parser( dictionary,
+                                        confparser,
                                         section):
   """ Use a ConfigParser to fill a dictionary.
-  
+
     Capture the text contained between two containers.
 
     Args:
       dictionary (dict): Dictionary to fill.
-      confparser (ConfigParser): ConfigParser Object where to take data for 
+      confparser (ConfigParser): ConfigParser Object where to take data for
         filling `dictionary`.
       section (str): Name of the section where to take data to fill the
         dictionary.
   """
   for entry in confparser.options(section):
     dictionary[entry] = confparser.get(section, entry)
+
+def str2bool(string, YES=["yes", "true", "t", "1"]):
+    """
+
+    Convert a string to a bool.
+
+    Args:
+      string (string): Name to convert.
+      YES (string): List of words accepted as true. They are no case sensitive.
+    """
+    return string.lower() in YES
